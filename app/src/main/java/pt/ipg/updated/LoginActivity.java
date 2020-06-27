fc8196d7032e868a4c29ff3c77d69a4ebcc38403
ivomcf <32466992+ivomcf@users.noreply.github.com>
2020-06-27 08:12:31 +0100

    Portugal Covid-19 Refeito, mas ainda trabalho em progresso.

---
+++ app/src/main/java/pt/ipg/updated/LoginActivity.java
@@ -0,0 +1,23 @@
+package pt.ipg.updated;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Bundle;
+import android.widget.RelativeLayout;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        RelativeLayout constraintLayout = findViewById(R.id.layout);
+        AnimationDrawable animationDrawable = (AnimationDrawable) constraintLayout.getBackground();
+        animationDrawable.setEnterFadeDuration(2000);
+        animationDrawable.setExitFadeDuration(4000);
+        animationDrawable.start();
+    }
+}
