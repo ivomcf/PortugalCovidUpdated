c8fd30278c828b5b50c9f51c5859aef5d2ffa2f0
ivomcf <32466992+ivomcf@users.noreply.github.com>
2020-06-27 12:10:50 +0100

    Apaguei TextViews.

--- app/src/main/java/pt/ipg/updated/MainActivity.java
+++ app/src/main/java/pt/ipg/updated/MainActivity.java
@@ -5,9 +5,12 @@ import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.material.tabs.TabItem;
 import com.google.android.material.tabs.TabLayout;
+
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
 
